e1bd72c155bcc25970e3a2a62de20966a6a40fcb
Brandon Knight <knightbrandon1984@gmail.com>
2022-10-17 20:21:32 -0500

    added the units=imperial so it reads in Fahrenheit and made a quick funct ion to see if I could get cityName to display - yes

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,10 @@
 // Open Weather API Key 'f65b142e1803ffb4f8713795f5973a88'
 const myAPIKey = 'f65b142e1803ffb4f8713795f5973a88';
 
-// Open Weather API Call
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={f65b142e1803ffb4f8713795f5973a88}
 
-// fetch(api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={f65b142e1803ffb4f8713795f5973a88})
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//       });
-
-// Geocoder API key 'f65b142e1803ffb4f8713795f5973a88'
-
-// Geocoder API Call
-//api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={API key}
-
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=f65b142e1803ffb4f8713795f5973a88')
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function (data) {
-//     console.log(data);
-//   });
-
-let myButton = document.getElementById('js-submit-city');
+// let myButton = document.getElementById('js-submit-city');
+// or I can do this
+let myButton = document.querySelector('#js-submit-city')
 
 myButton.addEventListener('click', getCityLatLon); 
 
@@ -37,7 +16,7 @@ myButton.addEventListener('click', getCityLatLon);
 
     //create API Geocode API string with city name
     // let geocodeAPIURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName +'&appid=f65b142e1803ffb4f8713795f5973a88'
-    const geocodeAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myAPIKey}`;
+    const geocodeAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myAPIKey}&units=imperial`;
 
 
     //pass API to a fetch call
@@ -48,6 +27,7 @@ myButton.addEventListener('click', getCityLatLon);
     .then(function (data) {
         // console.log(data);
         // console.log(data.coord.lat)
+        console.log(data.weather)
         const lat = data.coord.lat;
         const lon = data.coord.lon;
         getCityWeatherData(lat, lon);
@@ -57,21 +37,28 @@ myButton.addEventListener('click', getCityLatLon);
 function getCityWeatherData (latitude, longitude) {
     console.log(latitude, longitude);
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f65b142e1803ffb4f8713795f5973a88`)
+//changed the temp from the default of Kelvin to Fahrenheit added "&units=imperial" to the end - my result now reads in Fahrenheit
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f65b142e1803ffb4f8713795f5973a88&units=imperial`)
 .then(function (response) {
     return response.json();
 })
 .then(function (data) {
     console.log(data);
+    renderWeatherData(data);
 
   });
-
 }
 
 
 
-// Here is the example that I got from the site using my API key
-// - Example of API call:
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f65b142e1803ffb4f8713795f5973a88
+// city.name
+function renderWeatherData (cityClicked) {
+  let cityName = cityClicked.city.name; 
+  // console.log(cityName);
+  let searchedCity = document.querySelector('#searchedCity');
+  searchedCity.textContent = cityName;
+}
 
-// so I need to make a var for q to get my city, state it thats what I want in the above ex.
+
+// main.feels_like 
+// ///gives temp
